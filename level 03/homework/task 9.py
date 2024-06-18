1d5436009284406909ca735107dcb07325c4fb08
DavitAdeishvili <davitadeishvili31@gmail.com>
2024-06-18 12:46:31 +0400

    first commit

---
+++ level 03/homework/task 9.py
@@ -0,0 +1,2 @@
+# string is sentence or word in quatation marks.
+# number is integer that isnt in quatation marks.
---
+++ level 04/homework/task 5.py
@@ -0,0 +1,5 @@
+my_age = 12
+mum_age = 41
+me_in_10_years = my_age + 10
+mum_in_10_years = mum_age + 10
+print (mum_in_10_years / me_in_10_years)
