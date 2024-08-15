4876db6d829dedbbb36581be8de4bf4f08f5159d
DavitAdeishvili <davitadeishvili31@gmail.com>
2024-08-15 14:44:28 +0400

    Forty-third commit

---
+++ level 42/homework/task 2.py
@@ -0,0 +1,11 @@
+#replace
+
+something = "Best academy in the world is Mziuri"
+something = something.replace ("Mziuri", "GOA")
+print (something)
+
+#join
+
+list1 = ("David", "Adeishvili", "GOA")
+list1 = "#".join(list1)
+print(list1)
